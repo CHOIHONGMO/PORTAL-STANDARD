56d7c2e80907f0bbf0bf4b1fef32ac9a43aa2b0e
CHOIHONGMO <hmchoi73@gmail.com>
2026-06-15 13:35:30 +0900

    메뉴 변경

--- everportal/src/components/layout/Header.tsx
+++ everportal/src/components/layout/Header.tsx
@@ -27,32 +27,217 @@ const Header = () => {
         </div>
         <nav className="gnb">
           <ul>
-            <li><Link to="/about" className="nav-item">포털소개</Link></li>
-            <li><Link to="/service/issuance" className="nav-item">민원광장</Link></li>
-            <li><Link to="/board/notice" className="nav-item">정보마당</Link></li>
-            <li><Link to="/faq" className="nav-item">고객지원</Link></li>
-            <li><Link to="/qna" className="nav-item">Q&A</Link></li>
-            <li><Link to="/qustnr-respond" className="nav-item">설문참여</Link></li>
             <li className="dropdown">
-              <span className="nav-item dropdown-trigger">사이트 관리</span>
-              <ul className="dropdown-menu">
-                <li><Link to="/admin/member">회원관리</Link></li>
-                <li><Link to="/admin/board">게시판생성관리</Link></li>
-                <li><Link to="/admin/usage">게시판사용관리</Link></li>
-                <li><Link to="/admin/template">템플릿관리</Link></li>
-                <li><Link to="/admin/qna">Q&A답변관리</Link></li>
-                <li><Link to="/admin/qustnr">설문지관리</Link></li>
-                <li><Link to="/admin/qustnr-tmplat">설문템플릿관리</Link></li>
-                <li><Link to="/admin/qustnr-respond-info">설문응답결과</Link></li>
-                <li><Link to="/admin/qustnr-respond-manage">설문응답자정보</Link></li>
-                <li><Link to="/admin/policy">개인정보보호</Link></li>
-                <li><Link to="/admin/stplat">약관관리</Link></li>
-                <li style={{ borderTop: '1px solid var(--border-color)', margin: '4px 0' }}></li>
-                <li><Link to="/admin/role">롤관리</Link></li>
-                <li><Link to="/admin/author">권한관리</Link></li>
-                <li><Link to="/admin/group">그룹관리</Link></li>
-                <li><Link to="/admin/author-group">그룹권한관리</Link></li>
-              </ul>
+              <span className="nav-item dropdown-trigger">포털 소개 & 소식</span>
+              <div className="mega-dropdown cols-2">
+                <div className="mega-col">
+                  <h4 className="mega-col-title">포털 소개</h4>
+                  <ul className="mega-link-list">
+                    <li>
+                      <Link to="/about" className="mega-link-item">
+                        <span className="mega-link-title">사이트소개</span>
+                        <span className="mega-link-desc">포털 서비스와 비전을 소개합니다.</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/about/history" className="mega-link-item">
+                        <span className="mega-link-title">연혁</span>
+                        <span className="mega-link-desc">포털의 주요 발자취와 역사입니다.</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/about/organization" className="mega-link-item">
+                        <span className="mega-link-title">조직소개</span>
+                        <span className="mega-link-desc">조직도 및 업무 분장 정보입니다.</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/about/location" className="mega-link-item">
+                        <span className="mega-link-title">찾아오시는 길</span>
+                        <span className="mega-link-desc">오시는 길과 연락처 안내입니다.</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+                <div className="mega-col">
+                  <h4 className="mega-col-title">정보마당</h4>
+                  <ul className="mega-link-list">
+                    <li>
+                      <Link to="/board/notice" className="mega-link-item">
+                        <span className="mega-link-title">공지사항</span>
+                        <span className="mega-link-desc">포털의 주요 소식 및 공지사항입니다.</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+
+            <li className="dropdown">
+              <span className="nav-item dropdown-trigger">민원 & 설문</span>
+              <div className="mega-dropdown cols-2">
+                <div className="mega-col">
+                  <h4 className="mega-col-title">민원 서비스</h4>
+                  <ul className="mega-link-list">
+                    <li>
+                      <Link to="/service/issuance" className="mega-link-item">
+                        <span className="mega-link-title">민원발급</span>
+                        <span className="mega-link-desc">각종 증명서 및 민원 서류 발급</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/service/apply" className="mega-link-item">
+                        <span className="mega-link-title">민원신청</span>
+                        <span className="mega-link-desc">온라인 민원 신청 및 접수</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/service/result" className="mega-link-item">
+                        <span className="mega-link-title">민원결과확인</span>
+                        <span className="mega-link-desc">신청한 민원의 처리 결과를 조회합니다.</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+                <div className="mega-col">
+                  <h4 className="mega-col-title">의견 수렴</h4>
+                  <ul className="mega-link-list">
+                    <li>
+                      <Link to="/qustnr-respond" className="mega-link-item">
+                        <span className="mega-link-title">설문참여</span>
+                        <span className="mega-link-desc">설문조사에 참여하여 의견을 나누어 주세요.</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+
+            <li className="dropdown">
+              <span className="nav-item dropdown-trigger">고객지원</span>
+              <div className="mega-dropdown cols-1">
+                <div className="mega-col">
+                  <h4 className="mega-col-title">도움말 & 지원</h4>
+                  <ul className="mega-link-list">
+                    <li>
+                      <Link to="/faq" className="mega-link-item">
+                        <span className="mega-link-title">FAQ</span>
+                        <span className="mega-link-desc">자주 묻는 질문과 답변입니다.</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/qna" className="mega-link-item">
+                        <span className="mega-link-title">Q&A</span>
+                        <span className="mega-link-desc">궁금한 사항을 묻고 답하는 공간입니다.</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+
+            <li className="dropdown">
+              <span className="nav-item dropdown-trigger">시스템 관리</span>
+              <div className="mega-dropdown cols-4">
+                <div className="mega-col">
+                  <h4 className="mega-col-title">사용자 & 권한</h4>
+                  <ul className="mega-link-list">
+                    <li>
+                      <Link to="/admin/member" className="mega-link-item">
+                        <span className="mega-link-title">회원 관리</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/role" className="mega-link-item">
+                        <span className="mega-link-title">롤 관리</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/author" className="mega-link-item">
+                        <span className="mega-link-title">권한 관리</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/group" className="mega-link-item">
+                        <span className="mega-link-title">그룹 관리</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/author-group" className="mega-link-item">
+                        <span className="mega-link-title">그룹 권한 관리</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="mega-col">
+                  <h4 className="mega-col-title">게시판 & 콘텐츠</h4>
+                  <ul className="mega-link-list">
+                    <li>
+                      <Link to="/admin/board" className="mega-link-item">
+                        <span className="mega-link-title">게시판 생성 관리</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/usage" className="mega-link-item">
+                        <span className="mega-link-title">게시판 사용 관리</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/template" className="mega-link-item">
+                        <span className="mega-link-title">템플릿 관리</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/qna" className="mega-link-item">
+                        <span className="mega-link-title">Q&A 답변 관리</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="mega-col">
+                  <h4 className="mega-col-title">설문 관리</h4>
+                  <ul className="mega-link-list">
+                    <li>
+                      <Link to="/admin/qustnr" className="mega-link-item">
+                        <span className="mega-link-title">설문지 관리</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/qustnr-tmplat" className="mega-link-item">
+                        <span className="mega-link-title">설문 템플릿 관리</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/qustnr-respond-info" className="mega-link-item">
+                        <span className="mega-link-title">설문 응답 결과</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/qustnr-respond-manage" className="mega-link-item">
+                        <span className="mega-link-title">설문 응답자 정보</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="mega-col">
+                  <h4 className="mega-col-title">정책 & 약관</h4>
+                  <ul className="mega-link-list">
+                    <li>
+                      <Link to="/admin/policy" className="mega-link-item">
+                        <span className="mega-link-title">개인정보보호정책</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/stplat" className="mega-link-item">
+                        <span className="mega-link-title">약관 관리</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </li>
           </ul>
         </nav>
--- everportal/src/components/layout/ServiceLayout.tsx
+++ everportal/src/components/layout/ServiceLayout.tsx
@@ -1,64 +1,10 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 const ServiceLayout = () => {
   return (
-    <div className="container p_main" style={{ display: 'flex', gap: '40px', marginTop: '40px', minHeight: '600px' }}>
-      {/* Sidebar */}
-      <aside style={{ width: '220px', flexShrink: 0 }}>
-        <div className="glass" style={{ padding: '24px', borderRadius: 'var(--radius-lg)', border: '1px solid var(--border-color)', backgroundColor: 'var(--surface-color)' }}>
-          <h2 style={{ fontSize: '1.25rem', fontWeight: 800, marginBottom: '20px', color: 'var(--text-main)', borderBottom: '2px solid var(--primary-color)', paddingBottom: '10px' }}>민원광장</h2>
-          <ul style={{ listStyle: 'none', padding: 0, display: 'flex', flexDirection: 'column', gap: '12px' }}>
-            <li>
-              <NavLink 
-                to="/service/issuance" 
-                style={({ isActive }) => ({
-                  color: isActive ? 'var(--primary-color)' : 'var(--text-muted)',
-                  fontWeight: isActive ? '700' : '500',
-                  textDecoration: 'none',
-                  fontSize: '0.975rem',
-                  display: 'block',
-                  padding: '4px 0'
-                })}
-              >
-                민원발급
-              </NavLink>
-            </li>
-            <li>
-              <NavLink 
-                to="/service/apply" 
-                style={({ isActive }) => ({
-                  color: isActive ? 'var(--primary-color)' : 'var(--text-muted)',
-                  fontWeight: isActive ? '700' : '500',
-                  textDecoration: 'none',
-                  fontSize: '0.975rem',
-                  display: 'block',
-                  padding: '4px 0'
-                })}
-              >
-                민원신청
-              </NavLink>
-            </li>
-            <li>
-              <NavLink 
-                to="/service/result" 
-                style={({ isActive }) => ({
-                  color: isActive ? 'var(--primary-color)' : 'var(--text-muted)',
-                  fontWeight: isActive ? '700' : '500',
-                  textDecoration: 'none',
-                  fontSize: '0.975rem',
-                  display: 'block',
-                  padding: '4px 0'
-                })}
-              >
-                민원결과확인
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </aside>
-
+    <div className="container p_main" style={{ marginTop: '40px', minHeight: '600px' }}>
       {/* Main Content */}
-      <main style={{ flex: 1, minWidth: 0 }}>
+      <main style={{ minWidth: 0 }}>
         <Outlet />
       </main>
     </div>
